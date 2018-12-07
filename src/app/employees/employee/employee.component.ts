3487d1e4e407bd167fdd3462578b81e2e2eace44
novita <novitarahmaa11@gmail.com>
2018-12-06 18:46:50 -0800

    first commit

---
+++ src/app/employees/employee/employee.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
+import { EmployeeService } from '../shared/employee.service'; 
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private employeeService : EmployeeService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(employeeForm : NgForm){
+
+  }
+}
